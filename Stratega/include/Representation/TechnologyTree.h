8b9fa3f0711f025899f51a9d42b21988da85c6db
Jorge Hurtado <r4z31996@gmail.com>
2021-01-12 14:28:24 +0100

    Searching by int is 0 micro seconds

--- Stratega/include/Representation/TechnologyTree.h
+++ Stratega/include/Representation/TechnologyTree.h
@@ -65,6 +65,7 @@ namespace SGA
 			advancedArchery.description = "This is a advanced archery";
 			advancedArchery.name = "Advanced Archery";
 			advancedArchery.parentIDs.emplace_back(1);
+			advancedArchery.parentIDs.emplace_back(4);
 
 			TechnologyTreeNode moreAdvancedArchery;
 			moreAdvancedArchery.id = 3;
@@ -72,17 +73,25 @@ namespace SGA
 			moreAdvancedArchery.name = "More Advanced Archery";
 			moreAdvancedArchery.parentIDs.emplace_back(2);
 
+			TechnologyTreeNode goodBows;
+			goodBows.id = 4;
+			goodBows.description = "You need this before researching advanced archery";
+			goodBows.name = "Good Bows";
+			goodBows.parentIDs.emplace_back(0);
+
 			buildingTechnologyTreeType.nodes.emplace_back(townHall);
 			buildingTechnologyTreeType.nodes.emplace_back(archery);
 			buildingTechnologyTreeType.nodes.emplace_back(advancedArchery);
 			buildingTechnologyTreeType.nodes.emplace_back(moreAdvancedArchery);
+			buildingTechnologyTreeType.nodes.emplace_back(goodBows);
 
 			buildingTechnologyTreeType.technologies["TownHall"] = 0;
 			buildingTechnologyTreeType.technologies["Archery"] = 1;
 			buildingTechnologyTreeType.technologies["Advanced Archery"] = 2;
 			buildingTechnologyTreeType.technologies["More Advanced Archery"] = 3;
+			buildingTechnologyTreeType.technologies["Good Bows"] = 4;
 			
-			technologyTreeTypes["Building"]= buildingTechnologyTreeType;
+			technologyTreeTypes[0]= buildingTechnologyTreeType;
 
 			//Player 0 and 1 init
 			researchedTechnologies[0] = {};
@@ -92,7 +101,7 @@ namespace SGA
 			auto t1 = std::chrono::high_resolution_clock::now();
 			for (size_t i = 0; i < numberExecutions; i++)
 			{
-				canResearch(0, "Building", "Archery");
+				isResearched(0, 0, 1);
 			}
 			auto t2 = std::chrono::high_resolution_clock::now();
 
@@ -100,18 +109,41 @@ namespace SGA
 
 			std::cout << duration/numberExecutions<<std::endl;
 
-			/*research(0, "Building", "TownHall");
+			/*canResearch(0, 0, 1);
+			research(0, 0, 0);
 
-			canResearch(0, "Building", "Archery");*/
+			canResearch(0, 0, 1);
+			research(0, 0, 1);
+
+			canResearch(0, 0, 2);
+			canResearch(0, 0, 4);
+			research(0, 0, 4);			
+			canResearch(0, 0, 2);
+			research(0, 0, 2);*/
+
+			
 			
 		}
 		//TODO search by int 
 		//List of tech types
-		std::unordered_map<std::string, TechnologyTreeType> technologyTreeTypes;
+		std::unordered_map<int, TechnologyTreeType> technologyTreeTypes;
 		//PlayerID + pair of technology tree type id + the researched technology
-		std::unordered_map<int, std::vector<std::pair<std::string, std::string>>> researchedTechnologies;
+		std::unordered_map<int, std::vector<std::pair<int, int>>> researchedTechnologies;
 
-		bool isResearched(int playerID, std::string techologyTreeTypeName,std::string technologyName)
+		//bool isResearched(int playerID, std::string techologyTreeTypeName,std::string technologyName)
+		//{
+		//	//Search if the technology is found in the list of researchedtechnologies
+		//	auto& researchedPairList = researchedTechnologies.find(playerID);
+		//	for (auto& element : researchedPairList->second)
+		//	{
+		//		if (element.first == techologyTreeTypeName && element.second == technologyName)
+		//			return true;
+
+		//	}
+		//	return false;
+		//}
+
+		bool isResearched(int playerID, int techologyTreeTypeName, int technologyName)
 		{
 			//Search if the technology is found in the list of researchedtechnologies
 			auto& researchedPairList = researchedTechnologies.find(playerID);
@@ -124,16 +156,45 @@ namespace SGA
 			return false;
 		}
 
-		bool canResearch(int playerID, std::string techologyTreeTypeName, std::string technologyName)
+		//bool canResearch(int playerID, std::string techologyTreeTypeName, std::string technologyName)
+		//{
+		//	//Check if technology parents are researched
+		//	
+		//	//Get tech tree type
+		//	TechnologyTreeType& technologyTreeType = technologyTreeTypes[techologyTreeTypeName];
+
+		//	TechnologyTreeNode& technologyNode=technologyTreeType.searchNode(technologyName);
+
+		//	std::vector<int>& parentsIDs=technologyNode.parentIDs;
+
+		//	for (auto& parent : parentsIDs)
+		//	{
+
+		//		TechnologyTreeNode& technologyParentNode = technologyTreeType.searchNode(parent);
+
+		//		//TODO search by int 
+		//		if (!isResearched(playerID, techologyTreeTypeName,technologyParentNode.name))
+		//		{
+		//			//std::cout << "Cant research " << technologyName<<std::endl;
+		//			return false;
+		//		}
+		//			
+		//	}
+
+		//	//std::cout << "Can research " << technologyName << std::endl;
+		//	return true;
+		//}
+
+		bool canResearch(int playerID, int techologyTreeTypeName, int technologyName)
 		{
 			//Check if technology parents are researched
-			
+
 			//Get tech tree type
 			TechnologyTreeType& technologyTreeType = technologyTreeTypes[techologyTreeTypeName];
 
-			TechnologyTreeNode& technologyNode=technologyTreeType.searchNode(technologyName);
+			TechnologyTreeNode& technologyNode = technologyTreeType.searchNode(technologyName);
 
-			std::vector<int>& parentsIDs=technologyNode.parentIDs;
+			std::vector<int>& parentsIDs = technologyNode.parentIDs;
 
 			for (auto& parent : parentsIDs)
 			{
@@ -141,28 +202,44 @@ namespace SGA
 				TechnologyTreeNode& technologyParentNode = technologyTreeType.searchNode(parent);
 
 				//TODO search by int 
-				if (!isResearched(playerID, techologyTreeTypeName,technologyParentNode.name))
+				if (!isResearched(playerID, techologyTreeTypeName, technologyParentNode.id))
 				{
-					//std::cout << "Cant research " << technologyName<<std::endl;
+					//std::cout << "Cant research " << technologyNode.name <<std::endl;
 					return false;
 				}
-					
+
 			}
 
-			//std::cout << "Can research " << technologyName << std::endl;
+			//std::cout << "Can research " << technologyNode.name << std::endl;
 			return true;
 		}
 
-		void  research(int playerID, std::string techologyTreeTypeName, std::string technologyName)
+		//void  research(int playerID, std::string techologyTreeTypeName, std::string technologyName)
+		//{
+		//	//Search if the technology is found in the list of researchedtechnologies
+		//	auto& researchedPairList = researchedTechnologies.find(playerID);
+
+
+		//	//Get tech tree type
+		//	TechnologyTreeType& technologyTreeType = technologyTreeTypes[techologyTreeTypeName];
+
+		//	TechnologyTreeNode& technologyNode = technologyTreeType.searchNode(technologyName);
+
+		//	std::cout << "Researched " << technologyName << std::endl;
+		//	//Find technology index and add it to the researched list			
+		//	researchedPairList->second.emplace_back(techologyTreeTypeName, technologyName);
+		//}
+
+		void  research(int playerID, int techologyTreeTypeName, int technologyName)
 		{
 			//Search if the technology is found in the list of researchedtechnologies
 			auto& researchedPairList = researchedTechnologies.find(playerID);
 
 
-			//Get tech tree type
-			TechnologyTreeType& technologyTreeType = technologyTreeTypes[techologyTreeTypeName];
+			////Get tech tree type
+			//TechnologyTreeType& technologyTreeType = technologyTreeTypes[techologyTreeTypeName];
 
-			TechnologyTreeNode& technologyNode = technologyTreeType.searchNode(technologyName);
+			//TechnologyTreeNode& technologyNode = technologyTreeType.searchNode(technologyName);
 
 			std::cout << "Researched " << technologyName << std::endl;
 			//Find technology index and add it to the researched list			
