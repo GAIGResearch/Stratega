43bc56b65b61e09d3268eb671d95b1dc21ee84d5
Dominik Jeurissen <dominik.jeurissen@student.maastrichtuniversity.nl>
2021-02-01 17:48:49 +0100

    Fix configfiles + Cleanup assets

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -13,9 +13,9 @@ int main()
 	// Read Config
 	std::mt19937 engine(0ll);
 	SGA::GameConfigParser parser;
-	std::filesystem::path configPath2("../../../gameConfigs/ProtectTheBase.yaml");
-	auto yamlConfig = YAML::LoadFile(configPath2.string());
-	auto gameConfig = parser.parseFromFile(configPath2.string());
+	std::filesystem::path configPath("../../../gameConfigs/KillTheKing.yaml");
+	auto yamlConfig = YAML::LoadFile(configPath.string());
+	auto gameConfig = parser.parseFromFile(configPath.string());
 	auto renderConfig = yamlConfig.as<SGA::RenderConfig>();
 	
 	//// Initialize the game
@@ -58,7 +58,7 @@ int main()
 	// Initialize the gameRenderer
 	// We change the current_path to load sprites relative to the folder containing the configuration file
 	auto tmp = std::filesystem::current_path();
-	current_path(absolute(configPath2.parent_path()));
+	current_path(absolute(configPath.parent_path()));
 	auto stateRenderer = std::shared_ptr<SGA::GameStateRenderBase>(stateRendererFromConfig(*game, renderConfig, gameConfig, humanPlayerID));
 	
 	current_path(tmp);
--- GUI/src/RTSGameStateRender.cpp
+++ GUI/src/RTSGameStateRender.cpp
@@ -66,8 +66,8 @@ namespace SGA
 		}
 
 		// TODO Depends on location of configuration file, how to prevent that?
-		assetCache.loadTexture("circleCollider", "../GUI/Assets/circleColliderFixed.png");
-		assetCache.loadTexture("boxCollider", "../GUI/Assets/boxColliderFixed.png");
+		assetCache.loadTexture("circleCollider", "../GUI/Assets/circleCollider.png");
+		assetCache.loadTexture("boxCollider", "../GUI/Assets/boxCollider.png");
 		assetCache.loadFont("font", "../GUI/Assets/arial.ttf");
 
 		//Create new renderTexture
