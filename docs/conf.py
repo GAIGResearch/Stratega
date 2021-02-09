0a6fb3db0e99ff150e845044cf3a84642b2fdb09
Dominik Jeurissen <dominik.jeurissen@student.maastrichtuniversity.nl>
2021-02-09 10:55:02 +0100

    (Hopefully) Fix doxygen error in readTheDocs

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,29 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# -- Manually execute Doxygen, since ReadTheDocs doesn't execute our CMake file
+import subprocess, os
+
+def configureDoxyfile(input_dir, output_dir):
+    with open('Doxyfile.in', 'r') as file:
+        filedata = file.read()
+
+    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+    filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
+
+    with open('Doxyfile', 'w') as file:
+        file.write(filedata)
+
+# Check if we're running on Read the Docs' servers
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+breathe_projects = {}
+if read_the_docs_build:
+    input_dir = '../Stratega'
+    output_dir = 'build'
+    configureDoxyfile(input_dir, output_dir)
+    subprocess.call('doxygen', shell=True)
+    breathe_projects['Stratega'] = output_dir + '/xml'
+
 
 # -- Project information -----------------------------------------------------
 
