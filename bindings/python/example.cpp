5ee95769caadee79862e155e2b632d348e3d6bfc
Dominik Jeurissen <10519507+CommanderCero@users.noreply.github.com>
2021-03-21 19:32:32 +0100

    Compile example file with pybind & test importing stratega

---
+++ bindings/python/example.cpp
@@ -0,0 +1,12 @@
+#include <pybind11/pybind11.h>
+#include <Stratega/ForwardModel/Action.h>
+
+int add(int i, int j) {
+    return i + j;
+}
+
+PYBIND11_MODULE(example, m) {
+    m.doc() = "pybind11 example plugin"; // optional module docstring
+
+    m.def("add", &add, "A function which adds two numbers");
+}
