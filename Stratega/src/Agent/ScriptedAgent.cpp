48a4891f6a95a0372be8a5da280c0aa626635b8f
Alexander Dockhorn <alexander.dockhorn@ovgu.de>
2021-07-07 11:07:53 +0100

    added scripted agent demo

---
+++ Stratega/src/Agent/ScriptedAgent.cpp
@@ -0,0 +1,14 @@
+#include <random>
+#include <Stratega/Agent/ScriptedAgent.h>
+
+namespace SGA
+{
+	ActionAssignment ScriptedAgent::computeAction(GameState state, const ForwardModel* forwardModel, long timeBudgetMs)
+	{
+		// Generate all available actions
+		auto actionSpace = forwardModel->generateActions(state, getPlayerID());
+
+		// Return the action selected by the script
+		return ActionAssignment::fromSingleAction(actionScript_->getAction(state, actionSpace));
+	}
+}
