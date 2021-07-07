52ba52026bfc855e2caf9d63a7ca7d44ef0a22be
Alexander Dockhorn <alexander.dockhorn@ovgu.de>
2021-07-07 11:04:43 +0100

    added scripts

---
+++ Stratega/include/Stratega/Agent/ActionScripts/RunAwayFromOpponentScript.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <Stratega/Agent/ActionScripts/BaseActionScript.h>
+#include <set>
+#include <map>
+
+
+namespace SGA {
+	class RunAwayFromOpponentScript : public BaseActionScript
+	{
+
+	public:
+		RunAwayFromOpponentScript() : BaseActionScript() {};
+
+		Action getAction(const GameState& gameState, std::vector<Action>& actionSpace) const override;
+		Action getActionForUnit(const GameState& gameState, std::vector<Action>& actionSpace, int unitID) const override;
+		[[nodiscard]] std::string toString() const override { return "RunAwayFromOpponentScript"; };
+
+
+	private:
+		static double minimalDistanceToOpponents(const Vector2f position, std::map<int, Vector2f>& unitPositions, const std::set<int>& opponentUnits);
+
+	};
+
+}
