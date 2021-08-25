220abbfdf05bf3a67fe8a64386b44b528f4fd9c8
Jorge Hurtado <r4z31996@gmail.com>
2021-08-25 08:20:11 +0200

    Test without nodes

--- core/src/Agent/AgentFactory.cpp
+++ core/src/Agent/AgentFactory.cpp
@@ -62,12 +62,12 @@ namespace SGA
 		// Register agents available in the Stratega framework
 		factory.registerAgent<DoNothingAgent>("DoNothingAgent");
 		factory.registerAgent<RandomAgent>("RandomAgent");
-		factory.registerAgent<BFSAgent>("BFSAgent");
+		//factory.registerAgent<BFSAgent>("BFSAgent");
 		factory.registerAgent<RHEAAgent, RHEAParameters>("RHEAAgent");
 		factory.registerAgent<OSLAAgent>("OSLAAgent");
-		factory.registerAgent<BeamSearchAgent>("BeamSearchAgent");
+		/*factory.registerAgent<BeamSearchAgent>("BeamSearchAgent");
 		factory.registerAgent<DFSAgent>("DFSAgent");
-		factory.registerAgent<MCTSAgent, MCTSParameters>("MCTSAgent");
+		factory.registerAgent<MCTSAgent, MCTSParameters>("MCTSAgent");*/
 		factory.registerAgent<CombatAgent>("CombatAgent");
 		
 		return factory;
