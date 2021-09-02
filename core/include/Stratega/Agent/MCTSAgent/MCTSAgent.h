dafb7e92c92f3a91ed8f4735a1b9611b0abdbcd7
Jorge Hurtado <r4z31996@gmail.com>
2021-09-02 09:00:01 +0200

    Test with MCTS

--- core/include/Stratega/Agent/MCTSAgent/MCTSAgent.h
+++ core/include/Stratega/Agent/MCTSAgent/MCTSAgent.h
@@ -1,39 +1,39 @@
-//#pragma once
-//#include <Stratega/Agent/Agent.h>
-//#include <Stratega/Agent/MCTSAgent/MCTSNode.h>
-//#include <Stratega/Agent/Heuristic/AbstractHeuristic.h>
-//
-//#include "MCTSParameters.h"
-//
-//namespace SGA
-//{
-//	
-//	class MCTSAgent : public Agent
-//	{
-//	public:
-//		explicit MCTSAgent(const std::string& name, MCTSParameters&& params)
-//			:Agent{ name }, parameters_(std::move(params))
-//		{
-//		}
-//
-//		/// <summary>
-//		/// Override from Agent. This function is called at the start of the game only, providing a copy of the initial game state,
-//		/// a foward model, and a timer that indicates when the game will start.
-//		/// </summary>
-//		void init(GameState initialState, const ForwardModel& forwardModel, Timer timer) override;
-//
-//		/// <summary>
-//		/// Override from agents. This function is called at every tick of the game (turn in TBS, frame in RTS). The agent must return an
-//		/// action assignment object that indicates what is to be executed in the game. This function receives a copy of the game state, a 
-//		/// forward model and a timer that indicates when the action is due.  In TBS games, the game is
-//		/// blocked waiting for actions for eac agent, and penalties may apply if this time is not respected. In RTS games, 
-//		/// the game will continue independently of having returned an action, when the timer goes off.
-//		/// </summary>
-//		ActionAssignment computeAction(GameState state, const ForwardModel& forwardModel, Timer timer) override;
-//
-//	private:
-//		std::unique_ptr<MCTSNode> rootNode = nullptr;		//Root node of MCTS.
-//		int previousActionIndex = -1;						//Indicates the index of the action executed in the previous compute call.
-//		MCTSParameters parameters_;							//Configuration of MCTS.
-//	};
-//}
+#pragma once
+#include <Stratega/Agent/Agent.h>
+#include <Stratega/Agent/MCTSAgent/MCTSNode.h>
+#include <Stratega/Agent/Heuristic/AbstractHeuristic.h>
+
+#include "MCTSParameters.h"
+
+namespace SGA
+{
+	
+	class MCTSAgent : public Agent
+	{
+	public:
+		explicit MCTSAgent(const std::string& name, MCTSParameters&& params)
+			:Agent{ name }, parameters_(std::move(params))
+		{
+		}
+
+		/// <summary>
+		/// Override from Agent. This function is called at the start of the game only, providing a copy of the initial game state,
+		/// a foward model, and a timer that indicates when the game will start.
+		/// </summary>
+		void init(GameState initialState, const ForwardModel& forwardModel, Timer timer) override;
+
+		/// <summary>
+		/// Override from agents. This function is called at every tick of the game (turn in TBS, frame in RTS). The agent must return an
+		/// action assignment object that indicates what is to be executed in the game. This function receives a copy of the game state, a 
+		/// forward model and a timer that indicates when the action is due.  In TBS games, the game is
+		/// blocked waiting for actions for eac agent, and penalties may apply if this time is not respected. In RTS games, 
+		/// the game will continue independently of having returned an action, when the timer goes off.
+		/// </summary>
+		ActionAssignment computeAction(GameState state, const ForwardModel& forwardModel, Timer timer) override;
+
+	private:
+		std::unique_ptr<MCTSNode> rootNode = nullptr;		//Root node of MCTS.
+		int previousActionIndex = -1;						//Indicates the index of the action executed in the previous compute call.
+		MCTSParameters parameters_;							//Configuration of MCTS.
+	};
+}
--- core/src/Agent/MCTSAgent/MCTSAgent.cpp
+++ core/src/Agent/MCTSAgent/MCTSAgent.cpp
@@ -1,65 +1,65 @@
-//#include <Stratega/Agent/MCTSAgent/MCTSAgent.h>
-//
-//namespace SGA
-//{
-//
-//    void MCTSAgent::init(GameState initialState, const ForwardModel& /*forwardModel*/, Timer /*timer*/)
-//    {
-//        parameters_.PLAYER_ID = getPlayerID();
-//        if (parameters_.heuristic == nullptr)
-//            parameters_.heuristic = std::make_unique<AbstractHeuristic>(initialState);
-//        if (parameters_.budgetType == Budget::UNDEFINED)
-//            parameters_.budgetType = Budget::TIME;
-//        parameters_.opponentModel = std::make_shared<RandomActionScript>();
-//    }
-//
-//
-//    ActionAssignment MCTSAgent::computeAction(GameState state, const ForwardModel& forwardModel, Timer timer)
-//    {
-//        //Initialize the budget for this action call.
-//        parameters_.resetCounters(timer);
-//        //parameters_.printDetails();
-//
-//        // generate actions
-//        const auto actionSpace = forwardModel.generateActions(state, getPlayerID());
-//
-//        // if there is just one action and we don't spent the time on continuing our search
-//        // we just instantly return it
-//        // todo update condition to an and in case we can compare gameStates, since we currently cannot reuse the tree after an endTurnAction
-//        if (actionSpace.size() == 1 || !parameters_.continuePreviousSearch)
-//        {
-//            rootNode = nullptr;
-//            previousActionIndex = -1;
-//            return ActionAssignment::fromSingleAction(actionSpace.at(0));
-//        }
-//        else
-//        {
-//            const auto processedForwardModel = parameters_.preprocessForwardModel(forwardModel);
-//            if (parameters_.continuePreviousSearch && previousActionIndex != -1)
-//            {
-//                // in case of deterministic games we know which move has been done by us
-//                // reuse the tree from the previous iteration
-//                rootNode = std::move(rootNode->children.at(previousActionIndex));
-//                rootNode->parentNode = nullptr;	// release parent
-//                rootNode->setDepth(0);
-//            }
-//            else
-//            {
-//                // start a new tree
-//                rootNode = std::make_unique<MCTSNode>(*processedForwardModel, state, getPlayerID());
-//            }
-//
-//            //Do search!
-//            rootNode->searchMCTS(*processedForwardModel, parameters_, getRNGEngine());
-//
-//            // get and store best action
-//            auto bestActionIndex = rootNode->mostVisitedAction(parameters_, getRNGEngine());
-//            auto bestAction = rootNode->getActionSpace(forwardModel, getPlayerID()).at(bestActionIndex);
-//
-//            // return best action
-//            previousActionIndex = (bestAction.getActionFlag() == ActionFlag::EndTickAction) ? -1 : bestActionIndex;
-//            return ActionAssignment::fromSingleAction(bestAction);
-//        }
-//    }
-//
-//}
+#include <Stratega/Agent/MCTSAgent/MCTSAgent.h>
+
+namespace SGA
+{
+
+    void MCTSAgent::init(GameState initialState, const ForwardModel& /*forwardModel*/, Timer /*timer*/)
+    {
+        parameters_.PLAYER_ID = getPlayerID();
+        if (parameters_.heuristic == nullptr)
+            parameters_.heuristic = std::make_unique<AbstractHeuristic>(initialState);
+        if (parameters_.budgetType == Budget::UNDEFINED)
+            parameters_.budgetType = Budget::TIME;
+        parameters_.opponentModel = std::make_shared<RandomActionScript>();
+    }
+
+
+    ActionAssignment MCTSAgent::computeAction(GameState state, const ForwardModel& forwardModel, Timer timer)
+    {
+        //Initialize the budget for this action call.
+        parameters_.resetCounters(timer);
+        //parameters_.printDetails();
+
+        // generate actions
+        const auto actionSpace = forwardModel.generateActions(state, getPlayerID());
+
+        // if there is just one action and we don't spent the time on continuing our search
+        // we just instantly return it
+        // todo update condition to an and in case we can compare gameStates, since we currently cannot reuse the tree after an endTurnAction
+        if (actionSpace.size() == 1 || !parameters_.continuePreviousSearch)
+        {
+            rootNode = nullptr;
+            previousActionIndex = -1;
+            return ActionAssignment::fromSingleAction(actionSpace.at(0));
+        }
+        else
+        {
+            const auto processedForwardModel = parameters_.preprocessForwardModel(forwardModel);
+            if (parameters_.continuePreviousSearch && previousActionIndex != -1)
+            {
+                // in case of deterministic games we know which move has been done by us
+                // reuse the tree from the previous iteration
+                rootNode = std::move(rootNode->children.at(previousActionIndex));
+                rootNode->parentNode = nullptr;	// release parent
+                rootNode->setDepth(0);
+            }
+            else
+            {
+                // start a new tree
+                rootNode = std::make_unique<MCTSNode>(*processedForwardModel, state, getPlayerID());
+            }
+
+            //Do search!
+            rootNode->searchMCTS(*processedForwardModel, parameters_, getRNGEngine());
+
+            // get and store best action
+            auto bestActionIndex = rootNode->mostVisitedAction(parameters_, getRNGEngine());
+            auto bestAction = rootNode->getActionSpace(forwardModel, getPlayerID()).at(bestActionIndex);
+
+            // return best action
+            previousActionIndex = (bestAction.getActionFlag() == ActionFlag::EndTickAction) ? -1 : bestActionIndex;
+            return ActionAssignment::fromSingleAction(bestAction);
+        }
+    }
+
+}
