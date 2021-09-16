f6a647a5916b1e6de8adef7400ecf970b81d3c82
der4twu <m.aichmueller@gmail.com>
2021-09-16 17:41:34 +0200

    test workflow

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -2,7 +2,7 @@
 
 GTEST_API_ int main(int argc, char **argv)
 {
-    printf("Running main() from %s\n", __FILE__); 
+    printf("Running main() from %s\n", __FILE__);
     testing::InitGoogleTest(&argc, argv);
     testing::FLAGS_gtest_death_test_style = "threadsafe";
     return RUN_ALL_TESTS();
