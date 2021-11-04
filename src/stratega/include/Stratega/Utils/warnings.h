90820b96b3aff251cc8308d692d1eda176da676d
Jorge Hurtado <r4z31996@gmail.com>
2021-11-04 13:02:53 +0100

    Test

--- src/stratega/include/Stratega/Utils/warnings.h
+++ src/stratega/include/Stratega/Utils/warnings.h
@@ -20,7 +20,8 @@
 	#define DISABLE_WARNING_UNREFERENCED_FUNCTION            DISABLE_WARNING(-Wunused-function)
 	#define DISABLE_WARNING_UNREFERENCED					 DISABLE_WARNING(-Wunused)
 	#define DISABLE_WARNING_SIGN_CONVERSION					 DISABLE_WARNING(-Wsign-conversion)
-	#define DISABLE_WARNING_FORMAT				 DISABLE_WARNING(-Wformat-security)
+	#define DISABLE_WARNING_FORMAT							 DISABLE_WARNING(-Wformat-security)
+	#define DISABLE_WARNING_OLD_CAST						 DISABLE_WARNING(-Wold-style-cast)
 	// other warnings you want to deactivate... 
 
 
@@ -35,7 +36,8 @@
 	#define DISABLE_WARNING_UNREFERENCED					 DISABLE_WARNING(-Wunused)
 	#define DISABLE_WARNING_USELESS_CAST					 DISABLE_WARNING(-Wuseless-cast)
 	#define DISABLE_WARNING_SIGN_CONVERSION					 DISABLE_WARNING(-Wsign-conversion)
-#define DISABLE_WARNING_FORMAT				 DISABLE_WARNING(-Wformat-security)
+	#define DISABLE_WARNING_FORMAT							 DISABLE_WARNING(-Wformat-security)
+	#define DISABLE_WARNING_OLD_CAST						 DISABLE_WARNING(-Wold-style-cast)
 	// other warnings you want to deactivate... 
 
 #else
