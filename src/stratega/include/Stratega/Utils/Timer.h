595d7b412cc5ec1026f4b224a1159708a0b17a86
Jorge Hurtado <r4z31996@gmail.com>
2021-11-04 09:54:50 +0100

    Test

--- src/stratega/include/Stratega/Utils/Timer.h
+++ src/stratega/include/Stratega/Utils/Timer.h
@@ -76,7 +76,7 @@ namespace SGA
 				tempEndTime = endTime;
 			}
 
-			return static_cast<long>(std::chrono::duration_cast<std::chrono::milliseconds>(tempEndTime - startTime).count());
+			return /*static_cast<long>*/(std::chrono::duration_cast<std::chrono::milliseconds>(tempEndTime - startTime).count());
 		}
 
 		/// <summary>
