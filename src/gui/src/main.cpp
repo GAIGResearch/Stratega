7daf9c0b020de3f089a742493352184e85e210b1
Jorge Hurtado <r4z31996@gmail.com>
2022-02-21 12:00:51 +0000

    Parse simply expression

--- src/gui/src/main.cpp
+++ src/gui/src/main.cpp
@@ -11,15 +11,24 @@ int main()
 
 	cparse::TokenMap vars;
 	vars["pi"] = 3.14;
-	std::cout << cparse::calculator::calculate("-pi+1", vars) << std::endl; // 2.14
-	std::cout << cparse::calculator::calculate("pi + 1", vars) << std::endl; // 0.14
-	std::cout << cparse::calculator::calculate("pi-b == 3", vars) << std::endl; // 1 (true)
-	std::cout << cparse::calculator::calculate("sqrt(4) + 3**2 == 11") << std::endl; // 1 (true)
+	//std::cout << cparse::calculator::calculate("-pi+1", vars) << std::endl; // 2.14
+	//std::cout << cparse::calculator::calculate("pi + 1", vars) << std::endl; // 0.14
+	//std::cout << cparse::calculator::calculate("pi-b == 3", vars) << std::endl; // 1 (true)
+	//std::cout << cparse::calculator::calculate("sqrt(4) + 3**2 == 11") << std::endl; // 1 (true)
 
-	//std::mt19937 rngEngine(0);
-	////std::string configPath("../resources/gameConfigurations/RTS/BasicRTS.yaml");
-	//std::string configPath("../resources/gameConfigurations/TBS/KillTheKing.yaml");
-	//auto gameConfig = SGA::loadConfigFromYAML(configPath);
+	std::mt19937 rngEngine(0);
+	//std::string configPath("../resources/gameConfigurations/RTS/BasicRTS.yaml");
+	std::string configPath("../resources/gameConfigurations/TBS/KillTheKing.yaml");
+	auto gameConfig = SGA::loadConfigFromYAML(configPath);
+
+	std::string expressionTest = "Health*2";
+	auto entity = gameConfig->entityTypes[0].instantiateEntity(0);
+	for (auto& parameter : gameConfig->entityTypes[0].getParameters())
+	{
+		vars[parameter.second.getName()] = parameter.second.getDefaultValue();
+	}
+	std::cout << cparse::calculator::calculate(expressionTest.c_str(), vars) << std::endl;
+
 	//
 	//auto agents = gameConfig->generateAgents();
 	//// Set seed of the agents for deterministic behaviour - ToDo Should we move this into Stratega & Should it be done automatically with generateAgents?
