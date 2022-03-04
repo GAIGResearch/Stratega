7cd334954acf8cccb24e6241e1461848bc287506
Jorge Hurtado <r4z31996@gmail.com>
2022-03-04 06:44:46 +0000

    Updated pypi to 0.1.1

--- setup.py
+++ setup.py
@@ -126,7 +126,7 @@ with open(path.join(python_directory, "README.md")) as f:
 
 setup(
     name='Stratega',
-    version='0.1.0',
+    version='0.1.1',
     author='Diego Perez-Liebana, Alexander Dockhorn, Jorge Hurtado Grueso, Dominik Jeurissen',
     description='Stratega python bindings',
     long_description=long_description,
