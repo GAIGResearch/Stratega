acb01c8dff20f8833c8be29db5e2e1c451ac9328
Jorge Hurtado <r4z31996@gmail.com>
2022-03-18 11:04:27 +0000

    Test

--- cmake/targets/IMGUI.cmake
+++ cmake/targets/IMGUI.cmake
@@ -10,13 +10,19 @@ target_link_libraries(imgui
         PRIVATE
         CONAN_PKG::imgui
         # other platforms use Conan's `sfml`
-        "$<$<NOT:$<PLATFORM_ID:Linux>>:CONAN_PKG::sfml>"
         "$<$<PLATFORM_ID:Linux>:OpenGL>"
+
         "$<$<PLATFORM_ID:Windows>:CONAN_PKG::opengl>"
+        "$<$<PLATFORM_ID:Windows>:CONAN_PKG::sfml>"
+        #CONAN_PKG::sfml
         #"$<$<NOT:$<PLATFORM_ID:Linux>>:CONAN_PKG::opengl>"
         # for linux we have to use the targets of `sfml`'s components individually
         "$<$<PLATFORM_ID:Linux>:sfml-system>"
         "$<$<PLATFORM_ID:Linux>:sfml-graphics>"
         "$<$<PLATFORM_ID:Linux>:sfml-window>"
+        
+        "$<$<PLATFORM_ID:Darwin>:sfml-system>"
+        "$<$<PLATFORM_ID:Darwin>:sfml-graphics>"
+        "$<$<PLATFORM_ID:Darwin>:sfml-window>"
         #CONAN_PKG::opengl        
         )
