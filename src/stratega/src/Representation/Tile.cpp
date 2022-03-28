fbf0bdd22a43ca9a0e397c390a5b42d08a6dc990
Jorge Hurtado <r4z31996@gmail.com>
2022-03-28 10:34:41 +0100

    Added parameters to tiles structure

--- src/stratega/src/Representation/Tile.cpp
+++ src/stratega/src/Representation/Tile.cpp
@@ -5,8 +5,8 @@ namespace SGA
 	int Tile::getTileTypeID() const
 	{
 		int tileTypeID = -1;
-		if (tileType)
-			tileTypeID = tileType->getID();
+		if (type)
+			tileTypeID = type->getID();
 
 		return tileTypeID;
 	}
@@ -14,9 +14,64 @@ namespace SGA
 
 	const std::string Tile::name() const {
 	
-		if(tileType != nullptr)
-			return tileType->getName();
+		if(type != nullptr)
+			return type->getName();
 		return "Fog";
 	}
 
+
+	double Tile::getParameter(const std::string& paramName) const
+	{
+		for (const auto& param : type->getParameters()) {
+			if (param.second.getName() == paramName) {
+
+				auto it = parameters.find(param.second.getIndex());
+				if (it != parameters.end())
+					return it->second;
+				else
+					throw std::runtime_error("Parameter not found");
+			}
+		}
+		return -1;
+	}
+
+	double& Tile::getRawParameter(const std::string& paramName)
+	{
+		for (const auto& param : type->getParameters())
+		{
+			if (param.second.getName() == paramName)
+			{
+				return parameters[param.second.getIndex()];
+			}
+		}
+
+		throw std::runtime_error("Parameter not found");
+	}
+
+
+	std::vector<std::string> Tile::getTileParameterNames() const
+	{
+		std::vector<std::string> paramNames;
+		for (const auto& param : type->getParameters())
+			paramNames.emplace_back(param.second.getName());
+
+		return paramNames;
+	}
+
+
+	std::unordered_map<std::string, double> Tile::getTileParameters() const
+	{
+		std::unordered_map<std::string, double> params;
+		for (const auto& param : type->getParameters())
+		{
+			auto it = parameters.find(param.second.getIndex());
+			if (it != parameters.end())
+				params.emplace(param.second.getName(), it->second);
+			else
+				throw std::runtime_error("Parameter not found");
+		}
+
+
+		return params;
+	}
 }
