9f4ae3c836878c20b83bfec7544e13bb92656b74
Jorge Hurtado <r4z31996@gmail.com>
2022-04-13 11:16:37 +0100

    Updated main game

--- src/gui/src/main.cpp
+++ src/gui/src/main.cpp
@@ -11,8 +11,9 @@ int main()
 
 	boost::mt19937 rngEngine(0);
 	//std::string configPath("../resources/gameConfigurations/TBS/TheBattleOfStratega.yaml");
-	std::string configPath("../resources/gameConfigurations/RTS/Original/Settlers.yaml");
+	//std::string configPath("../resources/gameConfigurations/RTS/Original/Settlers.yaml");
 	//std::string configPath("../resources/gameConfigurations/TBS/Tests/KillTheKingQueueTest.yaml");
+	std::string configPath("../resources/gameConfigurations/TBS/Original/KillTheKing.yaml");
 	//std::string configPath("../resources/gameConfigurations/TBS/Ported/Pathway.yaml");
 	//std::string configPath("../resources/gameConfigurations/TBS/Ported/IntoTheBreach.yaml");
 	auto gameConfig = SGA::loadConfigFromYAML(configPath);
