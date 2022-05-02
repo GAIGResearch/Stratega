81fe69420f6e7f14306f2c0dfd447172709d827d
Jorge Hurtado <r4z31996@gmail.com>
2022-05-02 08:12:17 +0100

    Test

--- cmake/settings/Conan.cmake
+++ cmake/settings/Conan.cmake
@@ -13,6 +13,7 @@ macro(run_conan)
     conan_add_remote(NAME bincrafters URL
             https://api.bintray.com/conan/bincrafters/public-conan)
     conan_cmake_run(
+            ARCH armv8
             CONANFILE ${DEPENDENCY_DIR}/${CONANFILE}
             CONAN_COMMAND ${CONAN_PATH}
             ${CONAN_EXTRA_REQUIRES}
