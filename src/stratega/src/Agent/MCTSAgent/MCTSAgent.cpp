3ba09535b2b9037d68af70da3df01e421dd657d3
Linjie Xu <lin.svc@outlook.com>
2022-09-21 06:48:08 +0100

    experiments for pusher

--- src/stratega/src/Agent/MCTSAgent/MCTSAgent.cpp
+++ src/stratega/src/Agent/MCTSAgent/MCTSAgent.cpp
@@ -14,8 +14,8 @@ namespace SGA
             if (parameters_.heuristic == nullptr)
                 parameters_.heuristic = std::make_unique<AbstractHeuristic>(initialState);
         */
-        parameters_.heuristic = std::make_unique<AimToKingHeuristic>(initialState);
-        //parameters_.heuristic = std::make_unique<PushThemAllHeuristic>(getPlayerID(), initialState);
+        //parameters_.heuristic = std::make_unique<AimToKingHeuristic>(initialState);
+        parameters_.heuristic = std::make_unique<PushThemAllHeuristic>(getPlayerID(), initialState);
         if (parameters_.budgetType == Budget::UNDEFINED)
             parameters_.budgetType = Budget::TIME;
         parameters_.opponentModel = std::make_shared<RandomActionScript>();
