e609b194b8c4eb0ebf79742c5373231aa10986aa
Linjie Xu <lin.svc@outlook.com>
2022-09-28 15:11:00 +0100

    varaible usage in ntbea

--- src/stratega/include/Stratega/ntbea/Evaluators/Evaluator.h
+++ src/stratega/include/Stratega/ntbea/Evaluators/Evaluator.h
@@ -14,7 +14,7 @@ namespace SGA
     {
     protected:
         std::string _name;
-        std::unique_ptr<SearchSpace> _searchSpace;
+        std::unique_ptr<SGA::SearchSpace> _searchSpace;
 
     public:
         Evaluator(std::string name) :
@@ -26,7 +26,7 @@ namespace SGA
         virtual ~Evaluator() = default;
 
         [[nodiscard]] std::string getName() const { return _name; }
-        [[nodiscard]] SearchSpace* getSearchSpace() const { return _searchSpace.get(); }
+        [[nodiscard]] SGA::SearchSpace* getSearchSpace() const { return _searchSpace.get(); }
 
         virtual std::vector<float> evaluate(std::vector<int> point, int nSamples) = 0;
         virtual void printPoint(const std::vector<int>& point) = 0;
