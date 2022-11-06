7950f6ecbc561ee2455c86946153c0d297cfb031
Linjie Xu <lin.svc@outlook.com>
2022-11-06 01:38:34 +0000

    tune thresholds

--- src/ntbea/src/main.cpp
+++ src/ntbea/src/main.cpp
@@ -102,6 +102,7 @@ int main(int argc, char** argv)
         case 3: 
 			std::cout << "Optimize Elastic MCTSu Agent" << std::endl;
 
+            /*
             if (heuristic == "ktk") {
                 r_thresholds.push_back(0.05);
                 t_thresholds.push_back(1.0);
@@ -110,12 +111,13 @@ int main(int argc, char** argv)
                 r_thresholds.push_back(0.1);
                 t_thresholds.push_back(2.0);
             }
+            */
 			evaluator = std::make_unique<SGA::ElasMCTSuEvaluator>(
 				std::vector<float> {0.1, 1, 10, 100},               // values of k
 				std::vector<int> {  10, 20, 40},            // values of rollout
-				//std::vector<float> {0, 0.05, 0.1, 0.3, 0.5, 1.0},     // R threshold
-                //std::vector<float> {0, 0.5, 1.0, 1.5, 2.0},     // T threshold
-                r_thresholds, t_thresholds,
+				std::vector<float> {  0.1, 0.3, 0.5, 1.0, 1.5, 2.0},     // R threshold
+                std::vector<float> {0, 0.5, 1.0, 1.5, 2.0},     // T threshold
+                //r_thresholds, t_thresholds,
                 std::vector<int> {8},     // earlyStop
 				//std::vector<int> {2},
 				//std::vector<float> {0, 1, 5},					// magnitude values for each parameter
@@ -157,7 +159,9 @@ int main(int argc, char** argv)
 				std::vector<int> {  10, 20, 40},            // values of rollout
 				//std::vector<float> {0, 0.05, 0.1, 0.3, 0.5, 1.0},     // R threshold
                 //std::vector<float> {0, 0.5, 1.0, 1.5, 2.0},     // T threshold
-                r_thresholds, t_thresholds,
+                std::vector<float> {  0.1, 0.3, 0.5, 1.0, 1.5, 2.0},     // R threshold
+                std::vector<float> {0, 0.5, 1.0, 1.5, 2.0},     // T threshold
+                //r_thresholds, t_thresholds,
                 std::vector<int> {6, 8},     // earlyStop
 				//std::vector<int> {2},
 				//std::vector<float> {0, 1, 5},					// magnitude values for each parameter
